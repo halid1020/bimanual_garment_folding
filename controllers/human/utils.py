2a8485a607ba9711cad1257da02f77cb20181a5e
houdeyfa <houdeyfa.ajraou@gmail.com>
2026-01-20 12:04:09 +0000

    Changed the device ID to :1 (probably as I use an external monitor

--- controllers/human/utils.py
+++ controllers/human/utils.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-CV2_DISPLAY = ":0" #":0" # "localhost:10.0"
-SIM_DISPLAY =  ":0" #:0" # ""
+CV2_DISPLAY = ":1" #":0" # "localhost:10.0"
+SIM_DISPLAY =  ":1" #:0" # ""
 
 
 def apply_workspace_shade(rgb, mask, color, alpha=0.35):
